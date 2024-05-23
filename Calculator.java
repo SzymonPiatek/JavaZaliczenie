1df38512d375a8a624b8dc0fecc62256c6720be2
Szymon Piątek <szymon.piatek@pisf.pl>
2024-05-23 14:16:47 +0200

    Pętle sprawdzające

--- Calculator.java
+++ Calculator.java
@@ -4,15 +4,39 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Prosty kalkulator - możliwe operacje: +, -, *, /");
+        System.out.println("Prosty kalkulator do przeprowadzenia operacji na dwóch liczbach rzeczywistych - możliwe operacje: +, -, *, /");
         System.out.print("Podaj pierwszą liczbę: ");
-        double num1 = scanner.nextDouble();
+
+        double num1;
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Błąd: To nie jest liczba!");
+            System.out.print("Podaj pierwszą liczbę: ");
+            scanner.next();
+        }
+        num1 = scanner.nextDouble();
 
         System.out.print("Podaj symbol operacji arytmetycznej (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
+        char operator;
+        while (true) {
+            String input = scanner.next();
+            if (input.length() == 1 && "+-*/".indexOf(input.charAt(0)) != -1) {
+                operator = input.charAt(0);
+                break;
+            } else {
+                System.out.println("Błąd: Nieprawidłowy symbol operacji!");
+                System.out.print("Podaj symbol operacji arytmetycznej (+, -, *, /): ");
+            }
+        }
 
         System.out.print("Podaj drugą liczbę: ");
-        double num2 = scanner.nextDouble();
+
+        double num2;
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Błąd: To nie jest liczba!");
+            System.out.print("Podaj drugą liczbę: ");
+            scanner.next();
+        }
+        num2 = scanner.nextDouble();
 
         double result = 0;
         boolean validInput = true;
